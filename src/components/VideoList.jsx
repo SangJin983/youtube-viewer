ccd13cf40c8f39cce2bf785b24021eecff604970
SangJin983 <lazistar983@gmail.com>
2025-02-14 06:04:24 +0900

    refactor: excuteSearch, loadMoreVideo 개선 - excuteSearch의 함수명을 보다 일관성 있게 loadVideo로 개선 - useCallback으로 리랜더링시 함수가 재선언되어 생기는 문제를 방지 - THROTTLE_TIME과 같은 식으로, 상수를 컴포넌트 외부로 분리 - setInterval을 활용하여, 주기적으로 스크롤 위치 추적 (최하단에서 스크롤을 움직이지 않을시 무한 스크롤이 작동하지 않는 문제를 해결) - 일반적이고 다소 모호한 변수명들을 개선

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,40 +1,58 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchYouTubeSearchResults } from "../api/youtubeApi";
 import { appendVideos } from "../features/videoSlice";
 import { throttle } from "../utils/utils";
 import VideoItem from "./VideoItem";
 
+const THROTTLE_TIME = 5000;
+const SET_INTERVAL_TIME = 1000;
+
 const VideoList = () => {
   const dispatch = useDispatch();
-  const videos = useSelector((state) => state.videos.results);
+  const videos = useSelector((state) => state.videos.searchResult);
   const pageToken = useSelector((state) => state.videos.nextPageToken);
   const searchQuery = useSelector((state) => state.videos.searchQuery);
 
-  const loadMoreVideos = async () => {
-    const { items, nextPageToken } = await fetchYouTubeSearchResults(
-      searchQuery,
-      pageToken
-    );
-    const action = appendVideos({ items, nextPageToken });
-    dispatch(action);
-  };
+  const loadMoreVideos = useCallback(
+    async (searchTerm, pageToken) => {
+      const { items, nextPageToken } = await fetchYouTubeSearchResults(
+        searchTerm,
+        pageToken
+      );
+      const action = appendVideos({ items, nextPageToken });
+      dispatch(action);
 
-  const handleScroll = throttle(() => {
+      console.log("비디오를 추가합니다. 현재 searchTerm:", searchTerm);
+    },
+    [fetchYouTubeSearchResults, appendVideos, dispatch]
+  );
+
+  const throttledLoadMoreVideos = useCallback(
+    throttle(loadMoreVideos, THROTTLE_TIME),
+    [throttle, loadMoreVideos]
+  );
+
+  const handleScroll = () => {
     const scrollPosition = window.innerHeight + window.scrollY;
     const triggerPosition = document.body.offsetHeight - 500;
 
     // 현재 스크롤 위치가 트리거 위치에 도달했는지 확인
     if (scrollPosition >= triggerPosition) {
-      loadMoreVideos();
+      throttledLoadMoreVideos(searchQuery, pageToken);
     }
-  }, 500);
+  };
 
-  // handleScroll 이벤트 핸들러를 스크롤 이벤트에 등록
   useEffect(() => {
+    // 주기적으로 스크롤 위치 확인
+    const intervalId = setInterval(handleScroll, SET_INTERVAL_TIME);
+    // handleScroll 이벤트 핸들러를 스크롤 이벤트에 등록
     window.addEventListener("scroll", handleScroll);
     // 컴포넌트 언마운트 시 이벤트 핸들러 해제
-    return () => window.removeEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearInterval(intervalId);
+    };
   }, [handleScroll]);
 
   return (
