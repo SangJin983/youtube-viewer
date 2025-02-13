cef16a8e0dad3a0a6a5c8978b993a7d417a89e31
SangJin983 <lazistar983@gmail.com>
2025-02-14 05:50:31 +0900

    feat: fetchMockData 함수 추가 - 검색 카테고리별 mockData 추가 - gitIgnore로 mockData가 유출되는 것을 방지

--- src/api/youtubeApi.js
+++ src/api/youtubeApi.js
@@ -1,10 +1,47 @@
+import { getRandomIndices } from "../utils/utils";
+
 const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
-const BASE_URL = "https://www.googleapis.com/youtube/v3";
+const BASE_URL = import.meta.env.VITE_YOUTUBE_API_BASE_URL;
+const IS_USING_MOCK_DATA = true;
+const MOCK_DATA_FILE_NAMES = {
+  코딩: "coding",
+  요리: "cooking",
+  게임: "game",
+  영화: "movie",
+  음악: "music",
+  자연: "nature",
+  기술: "technology",
+  여행: "travel",
+};
+
+const fetchMockData = async (searchQuery) => {
+  const fileName = MOCK_DATA_FILE_NAMES[searchQuery];
+
+  if (fileName == null) {
+    throw new Error(`${searchQuery} does not match any available mock data`);
+  }
+
+  const res = await fetch(`/mocks/${fileName}.json`);
+  const data = await res.json();
+
+  const lastIndex = data.items.length - 1;
+  const size = Math.min(lastIndex, 10);
+  const randomIndices = getRandomIndices(0, lastIndex, size);
+  const randomItems = randomIndices.map((idx) => data.items[idx]);
+
+  console.log("fetch(mockData)가 실행됐습니다");
+
+  return { items: randomItems };
+};
 
 export const fetchYouTubeSearchResults = async (
   searchQuery,
   pageToken = ""
 ) => {
+  if (IS_USING_MOCK_DATA) {
+    return await fetchMockData(searchQuery);
+  }
+
   const params = new URLSearchParams({
     part: "snippet",
     q: searchQuery,
@@ -15,6 +52,7 @@ export const fetchYouTubeSearchResults = async (
   });
 
   const res = await fetch(`${BASE_URL}/search?${params}`);
+  console.log("fetch(YouTubeAPI)가 실행됐습니다");
 
   if (!res.ok) {
     const errorData = await res.json();
