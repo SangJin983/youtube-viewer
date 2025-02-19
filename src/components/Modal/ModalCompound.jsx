986b9e8a02880016ac1e13e85e154269aa71e93e
SangJin983 <lazistar983@gmail.com>
2025-02-19 10:02:08 +0900

    feat: 모달창 활성 시 스크롤 잠금 기능 추가

--- src/components/Modal/ModalCompound.jsx
+++ src/components/Modal/ModalCompound.jsx
@@ -8,8 +8,15 @@ export const ModalContext = createContext(null);
 const ModalCompound = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const open = useCallback(() => setIsOpen(true), [setIsOpen]);
-  const close = useCallback(() => setIsOpen(false), [setIsOpen]);
+  const open = useCallback(() => {
+    setIsOpen(true);
+    document.body.style.overflow = "hidden"; // 스크롤 잠금
+  }, [setIsOpen]);
+
+  const close = useCallback(() => {
+    setIsOpen(false);
+    document.body.style.overflow = "unset"; // 스크롤 잠금해제
+  }, [setIsOpen]);
 
   return (
     <ModalContext.Provider value={{ isOpen, open, close }}>
